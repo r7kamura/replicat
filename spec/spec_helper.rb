f910eed191d8391ddfeb4955da932d299fb51663
winebarrel <sgwr_dts@yahoo.co.jp>
2013-12-04 08:37:00 +0000

    sqlite -> mysql

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,21 +7,30 @@ require "rspec/rails"
 require "database_cleaner"
 
 RSpec.configure do |config|
+  copy_master_to_slave = proc do |adapter|
+    3.times do |i|
+      case adapter
+      when :mysql2
+        system("mysql -u root -e 'drop database dummy_test_slave#{i + 1}' > /dev/null 2> /dev/null")
+        system("mysql -u root -e 'create database dummy_test_slave#{i + 1}'")
+        system("mysqldump -u root dummy_test | mysql -u root dummy_test_slave#{i + 1}")
+      when :sqlite3
+        FileUtils.copy("#{Rails.root}/db/test.sqlite3", "#{Rails.root}/db/test_slave#{i + 1}.sqlite3")
+      end
+    end
+  end
+
   config.run_all_when_everything_filtered = true
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run :focus
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :deletion
-    3.times do |i|
-      FileUtils.copy("#{Rails.root}/db/test.sqlite3", "#{Rails.root}/db/test_slave#{i + 1}.sqlite3")
-    end
+    copy_master_to_slave.call(:mysql2)
   end
 
   config.after do
     DatabaseCleaner.clean
-    3.times do |i|
-      FileUtils.copy("#{Rails.root}/db/test.sqlite3", "#{Rails.root}/db/test_slave#{i + 1}.sqlite3")
-    end
+    copy_master_to_slave.call(:mysql2)
   end
 end
